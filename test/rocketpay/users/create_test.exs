9bfcad1e4f8ffeed83ae3e604c23be28c07963e3
Amorésio <a.junior.srlopes@gmail.com>
2021-02-28 19:39:50 -0400

    feat: teste de changeset

--- test/rocketpay/users/create_test.exs
+++ test/rocketpay/users/create_test.exs
@@ -1,2 +1,41 @@
 defmodule Rocketpay.Users.CreateTest do
+  use Rocketpay.DataCase
+
+  alias Rocketpay.User
+  alias Rocketpay.Users.Create
+
+  describe "call/1" do
+    test "when all params are valid, returns an user" do
+      params = %{
+        name: "junior",
+        password: "123456",
+        nickname: "lNooTD",
+        email: "amoresio@unemat.br",
+        age: 24
+      }
+
+      {:ok, %User{id: user_id}} = Create.call(params)
+      user = Repo.get(User, user_id)
+
+      assert %User{name: "junior", age: 24, id: ^user_id} = user
+    end
+
+    test "when there are invalids params, returns an user" do
+      params = %{
+        name: "junior",
+        nickname: "lNooTD",
+        email: "amoresio@unemat.br",
+        age: 14
+      }
+
+      {:error, changeset} = Create.call(params)
+
+      expected_response = %{
+        age: ["must be greater than or equal to 18"],
+        password: ["can't be blank"]
+      }
+
+      assert errors_on(changeset) == expected_response
+    end
+  end
 end
