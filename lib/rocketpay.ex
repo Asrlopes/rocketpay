8aca9676b4eeaf2441c7866cb02fc7c19b4e232c
Amorésio <a.junior.srlopes@gmail.com>
2021-02-28 15:48:45 -0400

    feat: adicionando a logica de deposito

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -1,5 +1,9 @@
 defmodule Rocketpay do
+  alias Rocketpay.Account.Deposit
+
   alias Rocketpay.Users.Create, as: UserCreate
 
   defdelegate create_user(params), to: UserCreate, as: :call
+
+  defdelegate deposit(params), to: Deposit, as: :call
 end
